64d21455bd280bc951d05d6929baaff64eef1832
Hari Om <hari333333om@gmail.com>
2025-02-26 18:21:23 +0530

    Added External Access

--- keepAlive.js
+++ keepAlive.js
@@ -73,4 +73,6 @@ app.get("/", (req, res) => {
   res.send("🚀 Keep-Alive Script is Running");
 });
 
-app.listen(PORT, () => console.log(`Health check running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Health check running on port ${PORT}`)
+);
